38cc90cdace54a583035d9bba2160ece70235703
mrshadowg7 <oscarurielcc@hotmail.com>
2025-02-25 12:40:08 -0600

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ onload = () =>{
         document.body.classList.remove("container");
 };
 
-const texto = "Para mi culona fav";
+const texto = "FELIZ CUMPLEAÑOS!!";
 const elementoTexto = document.getElementById("texto");
 let index = 0;
 
